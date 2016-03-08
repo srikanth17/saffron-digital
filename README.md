5fbe737c3cab7ae6e4836da162f3808870682301
Srikanth Dorairaj <srikanth@Srikanths-MacBook-Pro.local>
2016-03-08 09:08:08 +0000

    Adding documentation

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# saffron-digital
+# Github
+https://github.com/srikanth17/saffron-digital
+
+# Downloading Apache
+For setting up Apache, please follow the instructions from the link below.
+
+http://httpd.apache.org/docs/current/platform/
+
+Popular options for deploying Apache httpd
+Wamp Server (windows only) - http://www.wampserver.com
+XAMPP (Windows, Linux, OS X) - https://www.apachefriends.org/index.html
+
+# Starting Apache 
+Two ways to run Apache
+1. Running Apache as a Service 
+http://httpd.apache.org/docs/current/platform/windows.html
+The above link explains how to set up this as a Service.
+
+2. Running Apache as a Console Application
+http://httpd.apache.org/docs/current/platform/windows.html
+The above link explains how to set up this as a console Application.
+
+The apache server can be started with the command "apache start" after setting up using one of the above 2 ways.
+
+# Testing the Installation
+After Starting, the default listening port will be 80. To connect to the server and access the default page, launch a browser and enter this URL:
+
+http://localhost/ or http://127.0.0.1/
+
+Apache should respond with a welcome page and you should see "It Works!".
+
+# Running Application
+Place the downloaded folder (saffron-digital-master) inside the Apache Sites folder. Move to the corresponding URL on the web browser.
+
+http://localhost/saffron-digital-master/ or http://127.0.0.1/saffron-digital-master/
+
+Open the index.html file to see the application running. 
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,20 +1,23 @@
 var myApp = angular.module('myApp',[]);
+// This reads from sample.json file and updates the scope variables which are used in the view
 myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http) {
 	$http.get('sample.json').then(function(response) {
-		$scope.wins = response.data; 
+		$scope.wins = response.data; // this is an array of windows
 		$scope.width = [];
 		availableColors = ['progress-bar progress-bar-success','progress-bar progress-bar-info','progress-bar progress-bar-warning','progress-bar progress-bar-danger'];
-		startIndex = Math.floor(Math.random() * availableColors.length);
+		startIndex = 0;
 		$scope.colors = [];
 		$scope.maxWidth = 0;
 		$scope.maxIndex = 0;
-		setAllWidth();
+		setAllWidth(); // populate width array
 	});
 	
+	// This function takes an index and returns an index in availableColors so that it can be used a circular array
 	function getColorIndex(index) {
 		return index % availableColors.length;
 	}
 	
+	// Sets the width and color for each window
 	function setAllWidth() {
 		for(var i=0;i<$scope.wins.length;i++) {
 			setWidth(i);
@@ -22,6 +25,7 @@ myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http) {
 		}
 	}
 	
+	// Sets the width for window at index
 	function setWidth(index) {
 		if($scope.wins[index].start === "" || $scope.wins[index].end === "") {
 			$scope.width[index] = 0;
@@ -38,24 +42,24 @@ myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http) {
 		}
 	}
 	
+	// Prints window data on the console
 	$scope.getData = function() {
 		var temp = JSON.parse(angular.toJson($scope.wins));
 		console.log(JSON.stringify(temp,null,2));
-	}
+	};
 
-    $scope.addWindow = function() {
+    // Adds a window. Called when add window in clicked.
+	$scope.addWindow = function() {
 		$scope.wins.splice(0,0,{
-			name: $scope.name,
-			start: $scope.start,
-			end: $scope.end
+			name: "",
+			start: "",
+			end: ""
 		});
 		$scope.width.splice(0,0,0);
 		$scope.colors.splice(0,0,availableColors[getColorIndex(startIndex++)]);
-		$scope.name = "";
-		$scope.start = "";
-		$scope.end = "";
     };
 	
+	// Removes window at index. Called when close button is clicked.
 	$scope.removeWindow = function(index) {
 		$scope.wins.splice(index,1);	
 		$scope.width.splice(index,1);
@@ -65,9 +69,9 @@ myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http) {
 		for(var i=0;i<$scope.width.length;i++) {
 			setWidth(i);
 		}
-		$scope.$apply();
 	};
 	
+	// callback function when start date is changed for a window.
 	$scope.changeStartDate = function(index,date) {
 		$scope.wins[index].start = date;
 		$scope.maxWidth = 0;
@@ -78,6 +82,7 @@ myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http) {
 		$scope.$apply();
 	};
 	
+	// callback function when end date is changed for a window.
 	$scope.changeEndDate = function(index,date) {
 		$scope.wins[index].end = date;
 		$scope.maxWidth = 0;
@@ -89,6 +94,7 @@ myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http) {
 	};
 }]);
 
+// Directive for date picker ui for changing dates.
 myApp.directive('jqdatepicker', function () {
     return {
         restrict: 'A',
@@ -97,7 +103,7 @@ myApp.directive('jqdatepicker', function () {
             onChangeDate: '&',
 			index: '='
         },
-         link: function (scope, element, attrs, ngModelCtrl) {
+        link: function (scope, element, attrs, ngModelCtrl) {
             element.datepicker({
                 dateFormat: 'dd-mm-yy',
                 onSelect: function (date) {
@@ -110,15 +116,16 @@ myApp.directive('jqdatepicker', function () {
         }
     };
 });
-myApp.directive("progressbar", function () {
+
+// Directive for showing progress bar
+myApp.directive('progressbar', function () {
     return {
-        restrict: "A",
+        restrict: 'A',
         scope: {
-            total: "=",
-            current: "="
+            total: '=',
+            current: '='
         },
         link: function (scope, element) {
-
             scope.$watch("current", function (value) {
                 element.css("width", scope.current / scope.total * 100 + "%");
             });
