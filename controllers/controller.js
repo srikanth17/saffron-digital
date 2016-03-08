5c65e55e64d3ed7883e02b60eb92ced315c5f358
Srikanth Dorairaj <srikanth@Srikanths-MacBook-Pro.local>
2016-03-08 06:43:37 +0000

    Added functionality for the graphical bars

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,28 +3,39 @@ myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http) {
 	$http.get('sample.json').then(function(response) {
 		$scope.wins = response.data; 
 		$scope.width = [];
-		for(var i=0;i<$scope.wins.length;i++) {
-			setWidth(i);
-		}
+		availableColors = ['progress-bar progress-bar-success','progress-bar progress-bar-info','progress-bar progress-bar-warning','progress-bar progress-bar-danger'];
+		startIndex = Math.floor(Math.random() * availableColors.length);
+		$scope.colors = [];
+		$scope.maxWidth = 0;
+		$scope.maxIndex = 0;
+		setAllWidth();
 	});
 	
+	function getColorIndex(index) {
+		return index % availableColors.length;
+	}
+	
+	function setAllWidth() {
+		for(var i=0;i<$scope.wins.length;i++) {
+			setWidth(i);
+			$scope.colors[i] = availableColors[getColorIndex(startIndex++)];
+		}
+	}
+	
 	function setWidth(index) {
-		if($scope.wins[index].start === "" || $scope.wins[index].end === "")
+		if($scope.wins[index].start === "" || $scope.wins[index].end === "") {
+			$scope.width[index] = 0;
 			return;
+		}
 		var startArray = $scope.wins[index].start.split('-');
 		var endArray = $scope.wins[index].end.split('-');
 		var startDate = new Date(startArray[2],startArray[1]-1,startArray[0]);
 		var endDate = new Date(endArray[2],endArray[1]-1,endArray[0]);
-		var curDate = new Date();
-		if(startDate.getTime() >= curDate.getTime())
-			$scope.width[index] = 0;
-		else if(curDate.getTime() >= endDate.getTime())
-			$scope.width[index] = 100;
-		else {
-			var den = endDate.getTime() - startDate.getTime();
-			var num = curDate.getTime() - startDate.getTime();
-			$scope.width[index] = (num/den) * 100;
-		}	
+		$scope.width[index] = endDate.getTime() - startDate.getTime();
+		if($scope.maxWidth < $scope.width[index]) {
+			$scope.maxWidth = $scope.width[index];
+			$scope.maxIndex = index;
+		}
 	}
 	
 	$scope.getData = function() {
@@ -39,6 +50,7 @@ myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http) {
 			end: $scope.end
 		});
 		$scope.width.splice(0,0,0);
+		$scope.colors.splice(0,0,availableColors[getColorIndex(startIndex++)]);
 		$scope.name = "";
 		$scope.start = "";
 		$scope.end = "";
@@ -47,16 +59,33 @@ myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http) {
 	$scope.removeWindow = function(index) {
 		$scope.wins.splice(index,1);	
 		$scope.width.splice(index,1);
+		$scope.colors.splice(index,1);
+		$scope.maxWidth = 0;
+		$scope.maxIndex = 0;
+		for(var i=0;i<$scope.width.length;i++) {
+			setWidth(i);
+		}
+		$scope.$apply();
 	};
 	
 	$scope.changeStartDate = function(index,date) {
 		$scope.wins[index].start = date;
-		setWidth(index);
+		$scope.maxWidth = 0;
+		$scope.maxIndex = 0;
+		for(var i=0;i<$scope.width.length;i++) {
+			setWidth(i);
+		}
+		$scope.$apply();
 	};
 	
 	$scope.changeEndDate = function(index,date) {
 		$scope.wins[index].end = date;
-		setWidth(index);	
+		$scope.maxWidth = 0;
+		$scope.maxIndex = 0;
+		for(var i=0;i<$scope.width.length;i++) {
+			setWidth(i);
+		}	
+		$scope.$apply();
 	};
 }]);
 
@@ -78,6 +107,24 @@ myApp.directive('jqdatepicker', function () {
 					});
                 }
             });
+        }
+    };
+});
+myApp.directive("progressbar", function () {
+    return {
+        restrict: "A",
+        scope: {
+            total: "=",
+            current: "="
+        },
+        link: function (scope, element) {
+
+            scope.$watch("current", function (value) {
+                element.css("width", scope.current / scope.total * 100 + "%");
+            });
+            scope.$watch("total", function (value) {
+                element.css("width", scope.current / scope.total * 100 + "%");
+            })
         }
     };
 });
